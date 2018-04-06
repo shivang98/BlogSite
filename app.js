bf2dbdbabddc794d17f62e23cd6eb67781a38165
Shivang Agarwal <shivangagarwal98@gmail.com>
2018-04-06 11:46:37 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+var bodyParser = require("body-parser"),
+mongoose = require("mongoose"),
+express = require("express"),
+app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+mongoose.connect("mongodb://localhost/blog_site");
+
+var blogSchema = mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+// Blog.create({
+//     title: "test post",
+//     image: "https://www.w3schools.com/howto/img_fjords.jpg",
+//     body: "This is a great post"
+// }, function(err, blog){
+//     if(err){
+//         console.log(err);
+//     } else{
+//         console.log(blog);
+//     }
+// });
+
+app.get('/', function(req,res){
+    res.redirect('/blogs');
+});
+
+app.get('/blogs', function(req, res){
+    Blog.find({}, function(err, blogs){
+        if(err){
+            console.log(err);
+        } else{
+            // console.log(blogs);
+            res.render("index", {blogs: blogs});
+        }
+    });
+});
+
+app.listen(3000, function(){
+    console.log("BlogSite server up and running!");
+});
